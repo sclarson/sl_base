08dd515bac2f59e5d32bc87bb89dbe77dda9333c
Seth Larson <slarson@docutap.us>
2016-06-19 10:43:23 -0500

    Replace local paths with github

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -1,9 +1,13 @@
 require 'spec_helper'
 
 describe 'sl_base::default' do
+stub_command("which nginx").and_return('/usr/sbin/nginx')
   # Serverspec examples can be found at
   # http://serverspec.org/resource_types.html
-  it 'does something' do
-    skip 'Replace this with meaningful tests'
+  it 'installs the nginx package' do
+    chef_run.should install_package 'nginx'
   end
+
+  it 'runs apt-get update' do
+    chef_run.should execute_command 'apt-get update'
 end
